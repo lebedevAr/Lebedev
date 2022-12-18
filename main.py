8b32557e7a99ef74e2f6625e20a6a1dfb15dd2f0
Артём Лебедев <artem.lebedev@urfu.me>
2022-12-18 23:52:29 +0500

    3.2.2

--- main.py
+++ main.py
@@ -32,15 +32,15 @@ class Vacancy:
         Args:
             vacancy (dict): Словарь содержащий поля вакансии, необходиме для инициализации полей объекта Vacancy.
 
-        # >>> type(Vacancy({'name': 'Программист', 'salary_from': '20000', 'salary_to': '30000', 'salary_currency': 'RUR', 'area_name': 'Екатеринбург', 'published_at': '2020-11-03T17:11:35+0300'})).__name__
-		# 'Vacancy'
-		# >>> Vacancy({'name': 'Программист', 'salary_from': '20000', 'salary_to': '30000', 'salary_currency': 'RUR', 'area_name': 'Екатеринбург', 'published_at': '2020-11-03T17:11:35+0300'}).name
-		# 'Программист'
-		# >>> Vacancy({'name': 'Программист', 'salary_from': '20000', 'salary_to': '30000', 'salary_currency': 'RUR', 'area_name': 'Екатеринбург', 'published_at': '2020-11-03T17:11:35+0300'}).area_name
-		# 'Екатеринбург'
-		# >>> Vacancy({'name': 'Программист', 'salary_from': '20000', 'salary_to': '30000', 'salary_currency': 'RUR', 'area_name': 'Екатеринбург', 'published_at': '2020-11-03T17:11:35+0300'}).published_at
-		# '2007-12-03T17:34:36+0300'
-		# """
+        >>> type(Vacancy({'name': 'Программист', 'salary_from': '20000', 'salary_to': '30000', 'salary_currency': 'RUR', 'area_name': 'Екатеринбург', 'published_at': '2020-11-03T17:11:35+0300'})).__name__
+		'Vacancy'
+		>>> Vacancy({'name': 'Программист', 'salary_from': '20000', 'salary_to': '30000', 'salary_currency': 'RUR', 'area_name': 'Екатеринбург', 'published_at': '2020-11-03T17:11:35+0300'}).name
+		'Программист'
+		>>> Vacancy({'name': 'Программист', 'salary_from': '20000', 'salary_to': '30000', 'salary_currency': 'RUR', 'area_name': 'Екатеринбург', 'published_at': '2020-11-03T17:11:35+0300'}).area_name
+		'Екатеринбург'
+		>>> Vacancy({'name': 'Программист', 'salary_from': '20000', 'salary_to': '30000', 'salary_currency': 'RUR', 'area_name': 'Екатеринбург', 'published_at': '2020-11-03T17:11:35+0300'}).year
+		'2020'
+		"""
 
         self.name = vacancy['name']
         self.salary_from = int(float(vacancy['salary_from']))
@@ -66,15 +66,15 @@ class DataSet:
             file_name (str): название csv файла, расположенного в папке проекта
             vacancy_name (str): Название вакансии, по которой требуется.
 
-        # >>> DataSet('vacancies_by_year.csv', 'Программист').file_name
-		# 'vacancies_by_year.csv'
-		# >>> DataSet('empty.csv', 'Программист').file_name
-		# 'empty.csv'
-		# >>> DataSet('any.csv', 'Программист').vacancy_name
-		# 'Программист'
-		# >>> DataSet('any.csv', 'Аналитик').vacancy_name
-		# 'Аналитик'
-		# """
+        >>> DataSet('vacancies_by_year.csv', 'Программист').file_name
+		'vacancies_by_year.csv'
+		>>> DataSet('vacancies.csv', 'Программист').file_name
+		'vacancies.csv'
+		>>> DataSet('vacancies.csv', 'Аналитик').vacancy_name
+		'Аналитик'
+		>>> DataSet('vacancies.csv', 'Программист').vacancy_name
+		'Программист'
+		"""
 
         self.file_name = file_name
         self.vacancy_name = vacancy_name
@@ -241,13 +241,13 @@ class Report:
     def __init__(self, vacancy_name, stats1, stats2, stats3, stats4, stats5, stats6):
         """Инициализирует объект InputConnect.
 
-        # >>> Report({'2020': 30000, '2019': 35000}, {}, {}, {}, {}, {}).salary['2019']
-		# 35000
-		# >>> Report({}, {'2020': 1000, '2019': 700}, {}, {}, {}, {}).amount['2020']
-		# 1000
-		# >>> Report({}, {}, {}, {}, {}, {'Москва': 0.23, 'Екатеринбург': 0.11}).stats6['Москва']
-		# 0.23
-		# """
+        >>> Report({'2020': 30000, '2019': 35000}, {}, {}, {}, {}, {}).salary['2019']
+		35000
+		>>> Report({}, {'2020': 1000, '2019': 700}, {}, {}, {}, {}).amount['2020']
+		1000
+		>>> Report({}, {}, {}, {}, {}, {'Москва': 0.23, 'Екатеринбург': 0.11}).stats6['Москва']
+		0.23
+		"""
 
         self.wb = Workbook()
         self.vacancy_name = vacancy_name
